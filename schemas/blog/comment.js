67728713d3fd832cec80e967efdda28d677d5a34
Trung <laittg@gmail.com>
2019-07-20 09:50:13 +0700

    Add sample Mongoose schema

---
+++ schemas/blog/comment.js
@@ -0,0 +1,13 @@
+// Mongoose schema sample
+module.exports = {
+  title: String,
+  author: String,
+  body: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number
+  }
+}
