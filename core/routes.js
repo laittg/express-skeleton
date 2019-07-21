317fa59493108609fda5301999d748273622bb21
Trung <laittg@gmail.com>
2019-07-21 15:14:13 +0700

    Load and build routes by path convention

--- core/routes.js
+++ core/routes.js
@@ -1,12 +1,23 @@
 const path = require('path')
 const glob = require('glob')
 
+const routesDir = path.resolve(__dirname, '../routes')
+const apiPaths = glob.sync(path.resolve(routesDir, './**/!(blueprint).js'))
+
 var apis = []
 
-var apiPaths = glob.sync(path.resolve(__dirname, '../routes/**/!(blueprint).js'))
-
 apiPaths.map(apiPath => {
-  apis.push(require(apiPath))
+  var api = require(apiPath)
+  api.route = buildRoute(apiPath)
+  apis.push(api)
 })
+
+function buildRoute (apiPath) {
+  var route = apiPath
+    .substr(routesDir.length) // remove routesDir prefix
+    .slice(0, -3) // remove .js suffix
+    .replace(/\/index$/i, '') // convert /path/action/index --> /path/action
+  return route
+}
 
 module.exports = apis
