f4cd8523df3cbc58d9ed12fd5cb30a92430092fd
Trung <laittg@gmail.com>
2019-08-11 13:22:23 +0700

    Can use mongodb connection string from .env file

--- config/development/preload.js
+++ config/development/preload.js
@@ -4,7 +4,8 @@ async function preload () {
   // e.g. load credentials before everything else
   await new Promise((resolve, reject) => {
     setTimeout(resolve, 500)
-    process.env.MONGODB_CONNECTION_STRING = 'mongodb://localhost:27017/FirstClass'
+    process.env.MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING ||
+      'mongodb://localhost:27017/FirstClass'
   })
 }
 
